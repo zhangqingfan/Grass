4a11f76417f119654f9a5efeec06df56a9d30324
zhangqingfan <100165289+zhangqingfan@users.noreply.github.com>
2025-03-31 17:10:24 -0400

    1

---
+++ Assets/Script/Grass.shader
@@ -0,0 +1,50 @@
+﻿Shader "Grass"
+{
+    Properties
+    {
+        
+    }
+    SubShader
+    {
+        Tags { "RenderType"="Opaque" }
+        LOD 100
+
+        Pass
+        {
+            Cull Off
+
+            HLSLPROGRAM
+            #pragma vertex vert
+            #pragma fragment frag
+
+            #include "Packages/com.unity.render-pipelines.universal/ShaderLibrary/Core.hlsl"
+
+            struct appdata
+            {
+                float4 vertex : POSITION;
+                float2 uv : TEXCOORD0;
+            };
+
+            struct v2f
+            {
+                float2 uv : TEXCOORD0;
+                float4 vertex : SV_POSITION;
+            };
+
+            v2f vert (appdata v)
+            {
+                v2f o;
+                o.vertex = TransformObjectToHClip(v.vertex.xyz); 
+                o.uv = v.uv;
+                return o;
+            }
+
+            half4 frag (v2f i) : SV_Target
+            {   
+                half4 col = half4(1, 1, 1, 1);
+                return col;
+            }
+            ENDHLSL
+        }
+    }
+}
---
+++ Assets/Script/GrassMesh.cs
@@ -0,0 +1,82 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+using static UnityEngine.Rendering.HableCurve;
+
+[RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
+public class GrassMesh : MonoBehaviour
+{
+    [Range(1f, 2f)]
+    public float height = 1;
+    
+    [Range(0.2f, 0.6f)]
+    public float width = 0.3f;
+    
+    [Range(1, 5)]
+    public int segment = 1; //not include top point.
+
+    public Material mat;
+
+    void Start()
+    {
+        var verticesNum = (segment + 1) * 2 + 1;
+        var vertices = new Vector3[verticesNum];
+        var k = 0;
+        var segmentHeight = height / (segment + 1);
+
+        for(int i = 0; i <= segment; i++)
+        {
+            vertices[k] = new Vector3(-width / 2, i * segmentHeight, 0);
+            vertices[k + 1] = new Vector3(width / 2, i * segmentHeight, 0);
+            k += 2;
+        }
+        vertices[verticesNum - 1] = new Vector3(0, height, 0);
+
+        var triangleCount = (2 * segment + 1) * 3;
+        var triangles = new int[triangleCount];
+        k = 0;
+        for (int i = 0; i < segment; i++)
+        {
+            triangles[k] = i * 2 + 1;
+            triangles[k + 1] = i * 2;
+            triangles[k + 2] = i * 2 + 2;
+
+            triangles[k + 3] = i * 2 + 1;
+            triangles[k + 4] = i * 2 + 2;
+            triangles[k + 5] = i * 2 + 3;
+
+            k += 6;
+        }
+        triangles[k] = verticesNum - 2;
+        triangles[k + 1] = verticesNum - 3;
+        triangles[k + 2] = verticesNum - 1;
+
+        var colors = new Color[verticesNum];
+        for (int i = 0; i < verticesNum - 1; i++)
+        {
+            var r = (i % 2 == 0) ? 1 : 0; //1:left, 0:right, 0.5:middle
+            var g = (float)((int)(i / 2) / (segment + 1)); //height proportion
+            colors[i] = new Color(r, g, 0, 0);
+        }
+        colors[verticesNum - 1] = new Color(0.5f, 1, 0, 0);
+
+        var uvs = new Vector2[verticesNum];
+        for (int i = 0; i < verticesNum; i++)
+        {
+            //todo...
+            uvs[i] = new Vector2(0.5f, 0.5f);
+        }
+
+        Mesh mesh = new Mesh();
+        mesh.vertices = vertices;
+        mesh.triangles = triangles;
+        mesh.uv = uvs;
+        mesh.colors = colors;
+
+        var mf = gameObject.GetComponent<MeshFilter>();
+        mf.mesh = mesh;
+        var mr = gameObject.GetComponent<MeshRenderer>();
+        mr.material = mat;
+    }
+}
