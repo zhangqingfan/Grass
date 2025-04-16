fecb54f434377efe1e3f7bf12c958529b5149785
zhangqingfan <zhangqingfan2005@gmail.com>
2025-04-16 14:51:01 -0400

    1

--- Assets/Script/GrassGenerator.cs
+++ Assets/Script/GrassGenerator.cs
@@ -1,7 +1,6 @@
 using System.Collections.Generic;
 using System.Runtime.InteropServices;
 using Unity.Mathematics;
-using Unity.VisualScripting;
 using UnityEngine;
 
 public class GrassGenerator : MonoBehaviour
@@ -20,10 +19,10 @@ public class GrassGenerator : MonoBehaviour
     ComputeBuffer positionBuffer;
     ComputeBuffer colorBuffer;
     ComputeBuffer argsBuffer;
+    ComputeBuffer grassConfigBuffer;
     Bounds bounds;
 
-    ComputeBuffer debugBuffer;
-
+    //ComputeBuffer debugBuffer;
     int range = 100;
     float spacing = 0.5f;
 
@@ -31,24 +30,18 @@ public class GrassGenerator : MonoBehaviour
     readonly int spacingID = Shader.PropertyToID("_Spacing");
     readonly int worldPosBufferID = Shader.PropertyToID("worldPosBuffer");
     readonly int vpMatrixID = Shader.PropertyToID("_VP_MATRIX");
-    readonly int grassConfigBufferCount = Shader.PropertyToID("grassConfigBufferCount");
 
     public List<GrassConfig> grassConfigList = new List<GrassConfig>();
-    ComputeBuffer grassConfigBuffer;
+    readonly int grassConfigBufferCount = Shader.PropertyToID("grassConfigBufferCount");
     readonly int grassConfigBufferID = Shader.PropertyToID("grassConfigBuffer");
 
     void Start()
     {
-        worldPosBuffer = new ComputeBuffer(range * range, sizeof(float) * 3, ComputeBufferType.Append);
-        worldPosBuffer.SetCounterValue(0);
-        debugBuffer = new ComputeBuffer(range * range, sizeof(float) * 3, ComputeBufferType.Append);
-
+        //debugBuffer = new ComputeBuffer(range * range, sizeof(float) * 3, ComputeBufferType.Append);
         bounds = new Bounds(Vector3.zero, Vector3.one * 100000f);
-        argsBuffer = new ComputeBuffer(1, sizeof(int) * 4, ComputeBufferType.IndirectArguments);
 
-        grassConfigBuffer = new ComputeBuffer(grassConfigList.Count, Marshal.SizeOf<GrassConfig>());
-
-        CreateComputerBufferFromMesh(GrassMesh.Instance.mesh);
+        CreateComputerBuffers();
+        SyncShader();
         CreateVoronoiRT(voronoiRTSize.x, voronoiRTSize.y);
     }
 
@@ -56,63 +49,86 @@ public class GrassGenerator : MonoBehaviour
     {
         voronoiRT = new RenderTexture(width, height, 0, RenderTextureFormat.ARGBHalf, RenderTextureReadWrite.Linear);
         voronoiRT.Create();
-        computeShader.SetTexture(0, "voronoiRT", voronoiRT);
+        Graphics.Blit(null, voronoiRT, VoronoiMat); 
     }
-    
-    void CreateComputerBufferFromMesh(Mesh mesh)
+
+    void CreateComputerBuffers()
     {
+        var mesh = GrassMesh.Instance.mesh;
+
         triangleBuffer = new ComputeBuffer(mesh.triangles.Length, sizeof(int));
         triangleBuffer.SetData(mesh.triangles);
-        Shader.SetGlobalBuffer("triangleBuffer", triangleBuffer);
 
         uvBuffer = new ComputeBuffer(mesh.uv.Length, sizeof(float) * 2);
         uvBuffer.SetData(mesh.uv);
-        Shader.SetGlobalBuffer("uvBuffer", uvBuffer);
 
         colorBuffer = new ComputeBuffer(mesh.colors.Length, sizeof(float) * 4);
         colorBuffer.SetData(mesh.colors);
-        Shader.SetGlobalBuffer("colorBuffer", colorBuffer);
 
         positionBuffer = new ComputeBuffer(mesh.vertices.Length, sizeof(float) * 3);
         positionBuffer.SetData(mesh.vertices);
-        Shader.SetGlobalBuffer("vertexBuffer", positionBuffer);
 
-        grassMat.SetBuffer("worldPosBuffer", worldPosBuffer);  //可以放在上面？？
-    }
-
-    private void Update()
-    {
+        worldPosBuffer = new ComputeBuffer(range * range, sizeof(float) * 3, ComputeBufferType.Append);
         worldPosBuffer.SetCounterValue(0);
 
-        computeShader.SetInt(rangeID, range);
-        computeShader.SetFloat(spacingID, spacing); 
-        computeShader.SetBuffer(0, worldPosBufferID, worldPosBuffer);
+        grassConfigBuffer = new ComputeBuffer(grassConfigList.Count, Marshal.SizeOf<GrassConfig>());
+        grassConfigBuffer.SetData(grassConfigList.ToArray());
+    }
 
-        computeShader.SetInt(grassConfigBufferCount, grassConfigList.Count);
-        computeShader.SetBuffer(0, grassConfigBufferID, grassConfigBuffer);
+    void SyncShader()
+    {
+        //Grass shader;
+        Shader.SetGlobalBuffer("triangleBuffer", triangleBuffer);
+        Shader.SetGlobalBuffer("uvBuffer", uvBuffer);
+        Shader.SetGlobalBuffer("colorBuffer", colorBuffer);
+        Shader.SetGlobalBuffer("vertexBuffer", positionBuffer);
+        Shader.SetGlobalBuffer("worldPosBuffer", worldPosBuffer);
+        
+        //Voronoi Shader;
+        Shader.SetGlobalInt("_grassCount", grassCount);
+    }
+
+    void SyncComputeShader()
+    {
+        computeShader.SetInt(rangeID, range);
+        computeShader.SetFloat(spacingID, spacing);
+        computeShader.SetBuffer(0, worldPosBufferID, worldPosBuffer);
 
         var pMatrix = GL.GetGPUProjectionMatrix(renderCamera.projectionMatrix, false);
         var vpMatrix = pMatrix * renderCamera.worldToCameraMatrix;
         computeShader.SetMatrix(vpMatrixID, vpMatrix);
 
+        computeShader.SetInt(grassConfigBufferCount, grassConfigList.Count);
+        computeShader.SetBuffer(0, grassConfigBufferID, grassConfigBuffer);
+
+        computeShader.SetTexture(0, "voronoiRT", voronoiRT);
+    }
+
+    private void Update()
+    {
+        worldPosBuffer.SetCounterValue(0);
+        SyncComputeShader();
+
         var threadCountX = Mathf.CeilToInt(range / 8f);
         var threadCountZ = Mathf.CeilToInt(range / 8f);
         computeShader.Dispatch(0, threadCountX, threadCountZ, 1);
-
-        Shader.SetGlobalInt("_grassCount", grassCount);
-        Graphics.Blit(null, voronoiRT, VoronoiMat); //todo...临时代码，看效果用的
     }
 
     int[] args = new int[4];
-    private void LateUpdate()
+    void InitArgsBuffer()
     {
+        argsBuffer = argsBuffer == null ? new ComputeBuffer(1, sizeof(int) * 4, ComputeBufferType.IndirectArguments) : argsBuffer;
         args[0] = GrassMesh.Instance.mesh.triangles.Length;
         args[1] = 0;
         args[2] = 0;
         args[3] = 0;
         argsBuffer.SetData(args);
         ComputeBuffer.CopyCount(worldPosBuffer, argsBuffer, sizeof(int)); //must update worldPosbuffer count in every frame!!! 
+    }
 
+    private void LateUpdate()
+    {
+        InitArgsBuffer();
         Graphics.DrawProceduralIndirect(grassMat, bounds, MeshTopology.Triangles, argsBuffer, 0, null, null, UnityEngine.Rendering.ShadowCastingMode.Off, true);
     }
 
@@ -131,6 +147,6 @@ public class GrassGenerator : MonoBehaviour
         argsBuffer.Release();
         voronoiRT.Release();
         grassConfigBuffer.Release();
-        if (debugBuffer != null) debugBuffer.Release();
+        //if (debugBuffer != null) debugBuffer.Release();
     }
 }
